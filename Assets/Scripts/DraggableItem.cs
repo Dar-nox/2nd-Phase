44124bb8a1bb5b6f0ad8ab49221844d1901aef42
Conrad Arman De La Vega Vergara <flamingsunsgt@gmail.com>
2025-10-28 21:06:30 +0800

    Inventory dragging and stacking fixed

--- Assets/Scripts/DraggableItem.cs
+++ Assets/Scripts/DraggableItem.cs
@@ -5,13 +5,16 @@ using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using TMPro;
 
-public class DraggableItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+public class DraggableItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler, IPointerEnterHandler
 {
     public string itemID;
     public Image image;
     public TextMeshProUGUI countText;
     [HideInInspector] public Transform parentAfterDrag;
     public int count = 1;
+    [HideInInspector] public DraggableItem originalItem;
+    private bool isClone = false;
+    private bool isDraggingClone = false;
 
     private void Start()
     {
@@ -39,72 +42,110 @@ public class DraggableItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEn
         }
     }
 
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        // Nothing needed here anymore
+    }
+
     public void OnBeginDrag(PointerEventData eventData)
     {
-        // If this item is a stacked item, decrement the stack and drag a cloned single item instead.
+        // Check if this item has more than 1 count (works for both clones and originals)
         if (count > 1)
         {
-            // remember original parent so the clone can return to the same slot
-            Transform originalParent = transform.parent;
-
-            // remove one from the stack on the original
-            count -= 1;
-            UpdateCountDisplay();
-
-            // create a visual clone to drag
-            GameObject clone = Instantiate(gameObject, transform.root);
-            clone.name = gameObject.name; // remove (Clone) for clarity
-
-            // get the DraggableItem on the clone and set it up as a single item
-            DraggableItem cloneItem = clone.GetComponent<DraggableItem>();
-            if (cloneItem != null)
-            {
-                cloneItem.count = 1;
-                cloneItem.parentAfterDrag = originalParent;
-                cloneItem.UpdateCountDisplay();
-                if (cloneItem.image != null)
-                    cloneItem.image.raycastTarget = false;
-            }
-
-            // place the clone at the mouse and make it top-most
-            clone.transform.SetParent(transform.root);
-            clone.transform.SetAsLastSibling();
-            clone.transform.position = Input.mousePosition;
-
-            // transfer the pointer drag to the clone and send it a begin-drag event so it becomes the active drag target
-            eventData.pointerDrag = clone;
-            ExecuteEvents.Execute<IBeginDragHandler>(clone, eventData, ExecuteEvents.beginDragHandler);
-
-            return;
+            // Clone behavior for count > 1
+            Debug.Log("Creating clone for stacked item");
+            CreateCloneAndStartDrag(eventData);
+            isDraggingClone = true;
         }
-
-        Debug.Log("Started dragging item");
-        parentAfterDrag = transform.parent;
-        transform.SetParent(transform.root);
-        transform.SetAsLastSibling();
-        if (image != null)
+        else
+        {
+            // Normal drag behavior for count <= 1
+            Debug.Log("Started dragging item");
+            parentAfterDrag = transform.parent;
+            transform.SetParent(transform.root);
+            transform.SetAsLastSibling();
             image.raycastTarget = false;
+            isDraggingClone = false;
+        }
+    }
+
+    private void CreateCloneAndStartDrag(PointerEventData eventData)
+    {
+        // Store the parent before creating clone
+        parentAfterDrag = transform.parent;
+        
+        // Create a clone of this game object
+        GameObject cloneObj = Instantiate(gameObject, transform.parent);
+        
+        // Set the clone's RectTransform size to 150x150
+        RectTransform cloneRect = cloneObj.GetComponent<RectTransform>();
+        if (cloneRect != null)
+        {
+            cloneRect.sizeDelta = new Vector2(150, 150);
+        }
+        
+        // Get the clone's DraggableItem component
+        DraggableItem cloneItem = cloneObj.GetComponent<DraggableItem>();
+        if (cloneItem != null)
+        {
+            // Set clone properties
+            cloneItem.count = 1;
+            cloneItem.isClone = true;
+            cloneItem.originalItem = this;
+            cloneItem.UpdateCountDisplay();
+            cloneItem.parentAfterDrag = transform.parent;
+            
+            // Set up the clone for dragging
+            cloneItem.transform.SetParent(transform.root);
+            cloneItem.transform.SetAsLastSibling();
+            cloneItem.image.raycastTarget = false;
+            
+            // Manually set eventData.pointerDrag to the clone so the drag system works
+            eventData.pointerDrag = cloneObj;
+        }
+        
+        // Decrement the original item's count
+        count--;
+        UpdateCountDisplay();
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        // If this GameObject is not the active pointer drag target, ignore the drag events
-        if (eventData.pointerDrag != gameObject)
-            return;
-
-        Debug.Log("Dragging item");
-        transform.position = Input.mousePosition;
+        // Only drag if we're not creating a clone
+        if (!isDraggingClone)
+        {
+            Debug.Log("Dragging item");
+            transform.position = Input.mousePosition;
+        }
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        // If this GameObject is not the active pointer drag target, ignore the end-drag events
-        if (eventData.pointerDrag != gameObject)
-            return;
-
         Debug.Log("Ended dragging item");
-        transform.SetParent(parentAfterDrag);
-        if (image != null)
-            image.raycastTarget = true;
+        
+        // Only handle end drag if we're not creating a clone
+        if (!isDraggingClone)
+        {
+            // Check if clone was dropped successfully
+            if (isClone && transform.parent == parentAfterDrag)
+            {
+                // Clone was not placed in a new slot, destroy it and restore original count
+                if (originalItem != null)
+                {
+                    originalItem.count++;
+                    originalItem.UpdateCountDisplay();
+                }
+                Destroy(gameObject);
+            }
+            else
+            {
+                // Normal end drag behavior
+                transform.SetParent(parentAfterDrag);
+                image.raycastTarget = true;
+            }
+        }
+        
+        // Reset the flag
+        isDraggingClone = false;
     }
 }
--- Assets/Scripts/ItemSlot.cs
+++ Assets/Scripts/ItemSlot.cs
@@ -5,10 +5,63 @@ using UnityEngine.EventSystems;
 
 public class ItemSlot : MonoBehaviour, IDropHandler
 {
+    private void Update()
+    {
+        ConsolidateChildren();
+    }
+
+    private void ConsolidateChildren()
+    {
+        if (transform.childCount > 1)
+        {
+            // Keep the first child and stack the rest onto it
+            DraggableItem firstItem = transform.GetChild(0).GetComponent<DraggableItem>();
+            
+            if (firstItem != null)
+            {
+                int childrenToRemove = transform.childCount - 1;
+                
+                // Add the count of excess children to the first item
+                firstItem.count += childrenToRemove;
+                firstItem.UpdateCountDisplay();
+                
+                // Delete all children except the first one
+                for (int i = transform.childCount - 1; i > 0; i--)
+                {
+                    Destroy(transform.GetChild(i).gameObject);
+                }
+            }
+        }
+    }
+
     public void OnDrop(PointerEventData eventData)
     {
-        GameObject dropped = eventData.pointerDrag;
-        DraggableItem draggableItem = dropped.GetComponent<DraggableItem>();
-        draggableItem.parentAfterDrag = transform;
+        if (eventData.pointerDrag != null)
+        {
+            GameObject dropped = eventData.pointerDrag;
+            DraggableItem draggableItem = dropped.GetComponent<DraggableItem>();
+            
+            if (draggableItem == null) return;
+            
+            if (transform.childCount == 0)
+            {
+                // Slot is empty, place the item here
+                draggableItem.parentAfterDrag = transform;
+            }
+            else
+            {
+                // Slot already has an item - check if we can stack
+                DraggableItem existingItem = transform.GetChild(0).GetComponent<DraggableItem>();
+                
+                if (existingItem != null && existingItem.itemID == draggableItem.itemID)
+                {
+                    // Same item type, stack them
+                    existingItem.count += draggableItem.count;
+                    existingItem.UpdateCountDisplay();
+                    Destroy(dropped);
+                }
+                // If different items, do nothing (item will return to original position)
+            }
+        }
     }
 }
